e2304fde9a26dea90b426d820fc905074fa5d6b5
Daniel Bisiriyu <Bisiriyu97@gmail.com>
2020-07-19 12:49:08 +0100

    made changes to pagination

--- components/characterList/characterList.vue
+++ components/characterList/characterList.vue
@@ -7,9 +7,9 @@
       <div class="d-flex">
         <p class="my-auto mr-2">Filter:</p>
         <el-select v-model="gender" filterable placeholder="select">
-          <el-option label="Male" value="male"> </el-option>
-          <el-option label="Female" value="female"> </el-option>
-          <el-option label="Robot" value="robot"> </el-option>
+          <el-option label="Male" value="male"></el-option>
+          <el-option label="Female" value="female"></el-option>
+          <el-option label="Robot" value="robot"></el-option>
         </el-select>
       </div>
 
@@ -25,6 +25,8 @@
       <Pagination
         @previous="previous"
         @next="next"
+        :firstItemOnPage="firstItemOnPage"
+        :lastItemOnPage="lastItemOnPage"
         :totalResults="$store.state.allCharactersCount"
         :totalPages="totalPages"
       />
@@ -47,11 +49,21 @@ export default {
     return {
       gender: "",
       currentPage: 1,
-      totalPages: Math.ceil(this.$store.state.allCharactersCount / 10)
+      // totalPages: Math.ceil(this.$store.state.allCharactersCount / 10),
+      lastItemOnPage: null,
+      firstItemOnPage: null
     };
   },
   mounted() {
     this.$store.dispatch("getAllCharacters", this.currentPage);
+    if (this.totalPages == 1) {
+      this.firstItemOnPage = 1;
+      this.lastItemOnPage = this.$store.state.allCharactersCount;
+    } else {
+      // DEFAULT WHEN PAGE LOADS AND WE HAVE MORE THAN ONE PAGE
+      this.firstItemOnPage = 1;
+      this.lastItemOnPage = 10;
+    }
   },
   computed: {
     filteredCharacters() {
@@ -59,14 +71,32 @@ export default {
       return newList.filter(character => {
         return character.gender.includes(this.gender);
       });
+    },
+    totalPages() {
+      return Math.ceil(this.$store.state.allCharactersCount / 10);
+    },
+    totalItems() {
+      return this.$store.state.allCharactersCount;
     }
   },
   methods: {
     next() {
+      console.log("next called");
+      // console.log(this.currentPage, this.totalPages);
       if (this.currentPage < this.totalPages) {
+        // console.log("if statement passed");
         // console.log(this.totalPages);
         this.currentPage++;
         this.$store.dispatch("getAllCharacters", this.currentPage);
+      }
+      if (this.lastItemOnPage + 10 < this.totalItems) {
+        // PASSES IF THE LAST ITEM IS LESS THAN TOTAL MEANING MORE PAGES
+        this.firstItemOnPage = this.lastItemOnPage + 1;
+        this.lastItemOnPage = this.firstItemOnPage + 9;
+      } else {
+        // PASSES IF LAST ITEM IS GREATER MEANING NO MORE PAGES AFTER THIS ONE
+        this.firstItemOnPage = this.lastItemOnPage + 1;
+        this.lastItemOnPage = this.totalItems;
       }
     },
     previous() {
